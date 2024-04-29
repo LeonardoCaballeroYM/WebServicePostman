5ec1146b6ce4f0600414d3a74b15809a80ba3589
LeonardoCaballeroYM <leonardocaballero25@gmail.com>
2024-04-29 15:49:45 -0600

    Configuración de cosmos db con mongo db

--- src/test/java/com/example/WebService/WebServicePostmanApplicationTests.java
+++ src/test/java/com/example/WebService/WebServicePostmanApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.WebService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class WebServicePostmanApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void getMessageTest() {
+		ServidorWEBRest servidor = new ServidorWEBRest();
+        String resultado = servidor.imprimir();
+        assertEquals("Saludos desde spring boot <3", resultado);
 	}
 
 }
